a0f765ecb7d5ef16cf5d5879f403d01133d697e8
yangtianfu <yangtf01@beantechs.cn>
2021-03-05 16:47:06 +0800

    init

---
+++ app/src/main/java/com/bliss/yang/jetapp/LifeCycleListener.kt
@@ -0,0 +1,45 @@
+package com.bliss.yang.jetapp
+
+import android.app.Activity
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+/**
+* Author: yangtianfu
+* Date: 2021/3/5 16:30
+* Describe: 绑定activity的生命周期
+*/
+class LifeCycleListener(val activity: Activity,val onChangeListener: LifeCycleListener.OnChangeListener) :LifecycleObserver {
+    companion object{
+        init {
+            Log.e("tagLifeCycleListener", ": 执行" )
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    private fun onCreated(){
+        onChangeListener.onChange("回调 onCreated 执行")
+        Log.e("tagLifeCycleListener", ":onCreated 执行" )
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    private fun onResume(){
+        Log.e("tagLifeCycleListener", ":onResume 执行" )
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    private fun onPause(){
+        Log.e("tagLifeCycleListener", ":onPause 执行" )
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    private fun onDestroy(){
+        Log.e("tagLifeCycleListener", ":onDestroy 执行" )
+    }
+
+    interface OnChangeListener{
+        fun onChange(des:String)
+    }
+}
---
+++ app/src/main/java/com/bliss/yang/jetapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.bliss.yang.jetapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+/**
+* Author: yangtianfu
+* Date: 2021/3/5 16:19
+* Describe:
+*/
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private lateinit var lifecycleListener:LifeCycleListener
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.e(TAG, "onCreate: 执行", )
+        lifecycleListener = LifeCycleListener(this, object : LifeCycleListener.OnChangeListener {
+            override fun onChange(des: String) {
+                Log.e(TAG, "接受到 onChange: ")
+            }
+        })
+        //lifecycle将观察者和被观察者绑定，解决组件对activity生命周期的依赖问题
+        lifecycle.addObserver(lifecycleListener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e(TAG, "onResume: 执行", )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e(TAG, "onPause: 执行", )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "onDestroy: 执行", )
+    }
+}
